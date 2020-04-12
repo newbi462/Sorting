d906825c59688c02226c8dd52f8b845c31ece727
newbi462 <isthisemailfree@gmail.com>
2020-04-12 16:56:37 -0400

    bubble_sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -70,9 +70,22 @@ if len(test) > 0:
 """
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    run_loop = 0
+    while run_loop < len(arr):
+        for i in range(0, len(arr)-1):
+            print(arr)
+            if arr[i+1] < arr[i]:
+                print(f"move: {arr[i+1]}")
+                arr.insert(i, arr[i+1])
+                arr.pop(i+2)
+                run_loop = 0
+            else:
+                run_loop += 1
 
     return arr
 
+test = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+print(bubble_sort( test ))
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
